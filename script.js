5dc3744c917ec37126988d183c10ea9eb3b0d665
Samuel Njenga <snjenga704@gmail.com>
2022-09-23 22:04:03 +0300

    feat: modify script.js by including Notification for visibility change event listener

--- script.js
+++ script.js
@@ -21,3 +21,15 @@ button.addEventListener("click", () => {
     }
   });
 });
+
+let notification;
+document.addEventListener("visibilitychange", () => {
+  if (document.visibilityState === "hidden") {
+    notification = new Notification("Come back please", {
+      body: "PLEASEEE",
+      tag: "Come Back",
+    });
+  } else {
+    notification.close();
+  }
+});
