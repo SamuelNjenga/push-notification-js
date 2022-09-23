a922f8aeeec88be21d3aee2a5122fa5be889caa9
Samuel Njenga <snjenga704@gmail.com>
2022-09-23 21:29:29 +0300

    feat: modify script.js by including event listener on close of push notification

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
-  const notification = Notification.requestPermission().then((perm) => {
+  Notification.requestPermission().then((perm) => {
     if (perm === "granted") {
-      new Notification("Example notification", {
+      const notification = new Notification("Example notification", {
         body: "This is more text",
         data: {
           hello: "world",
         },
       });
+      notification.data;
+      notification.addEventListener("close", (e) => {
+        console.log(e);
+      });
     }
   });
-  notification.data;
 });
